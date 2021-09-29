374d6d458222d3f9539fb4e393db30dbdf722629
Ameerah Baabdullah <alabdullah28@yahoo.com>
2021-09-30 00:15:14 +0300

    THis is Final Code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,88 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            List<Teacher> lst1 = new List<Teacher>();
+            //Console.WriteLine("https://github.com/AmeerahBaabdullah/TeacherSystem");
+
+            string filepath = "d:/teacher.txt";
+
+
+            int c = 1;
+            do
+            {
+                Console.WriteLine("Enter Teacher Info:");
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Class: ");
+                string classn = Console.ReadLine();
+                Console.Write("Section:");
+                string section = Console.ReadLine();
+                lst1.Add(new Teacher(id, name, classn, section));
+
+
+                Console.WriteLine("Add Another? 1 for continue , any key for exit");
+                c = int.Parse(Console.ReadLine());
+
+
+
+            } while (c == 1);
+
+            //Add Teachers to File;
+            using (StreamWriter writer = new StreamWriter(filepath))
+            {
+                foreach (Teacher t in lst1)
+                {
+                    writer.WriteLine(t.BuildFileRow());
+                    Console.WriteLine(t.Name + "Added Success");
+                }
+            }
+
+
+
+            Console.WriteLine("All teacher Written on File " + filepath);
+
+
+            Console.WriteLine("Read From c:/ Teachers.txt");
+
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(filepath))
+                {
+                    string line;
+
+                    // Read and display lines from the file until 
+                    // the end of the file is reached. 
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Teacher t = new Teacher(line);
+                        t.printInfo();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+
+
+
+
         }
     }
 }
--- ConsoleApp1/Teacher.cs
+++ ConsoleApp1/Teacher.cs
@@ -1,10 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+
 
 namespace ConsoleApp1
 {
     class Teacher
     {
+
+        private int ID_;
+        private string Name_;
+
+        private string Section_;
+        private string ClassName_;
+
+        public Teacher()
+        {
+            Name_ = "0";
+            ID_ = 0;
+            ClassName_ = "";
+            Section_ = "";
+        }
+
+        public Teacher(int id, string n, string c, string s)
+        {
+            ID_ = id;
+            Name_ = n;
+            ClassName_ = c;
+            Section_ = s;
+        }
+        public Teacher(string filerow)
+        {
+            string[] arr = filerow.Split(',');
+
+            ID_ = Convert.ToInt32(arr[0]);
+            Name_ = arr[1];
+            ClassName_ = arr[2];
+            Section_ = arr[3];
+        }
+
+
+        // properties of attributes
+        public string Name
+        {
+            get { return Name_; }
+            set { Name_ = value; }
+        }
+        public int ID
+        {
+            get { return ID_; }
+            set { ID_ = value; }
+        }
+
+        public string ClassName
+        {
+            get { return ClassName_; }
+            set { ClassName_ = value; }
+        }
+
+        public string Section
+        {
+            get { return Section_; }
+            set { Section_ = value; }
+        }
+
+
+        public string BuildFileRow()
+        {
+
+            string s = "";
+            s += ID_.ToString();
+            s += ",";
+            s += Name_;
+            s += ",";
+            s += ClassName_;
+            s += ",";
+            s += Section_;
+
+            return s;
+        }
+
+        public void printInfo()
+        {
+            Console.Write("ID:" + ID);
+            Console.Write("\tName:" + Name);
+            Console.Write("\tSection:" + Section);
+            Console.Write("\tClass:" + ClassName_);
+            Console.WriteLine();
+
+        }
+
     }
 }
